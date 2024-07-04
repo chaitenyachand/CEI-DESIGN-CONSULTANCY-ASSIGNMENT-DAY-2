124dde6278a3a489bc583c8b4595687b4d73e29f
chaitenyachand <129392157+chaitenyachand@users.noreply.github.com>
2024-07-04 11:45:25 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,13 @@
+# Write a Python program that initializes an array with 5 numbers, adds a new number to the array, and prints the updated array.
+
+# Initialize an array with 5 numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Add a new number to the array
+numbers.append(6)
+
+# Print the updated array
+print(numbers)
+
+# Output
+#[1, 2, 3, 4, 5, 6]
